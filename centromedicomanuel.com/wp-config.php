d1fc44c85f308ed1519605458e9479ad9b59113d
Steven Ayvar <ayvarsteven@gmail.com>
2023-10-19 14:02:08 -0500

    adds database credentials for staging and production environments

--- centromedicomanuel.com/wp-config.php
+++ centromedicomanuel.com/wp-config.php
@@ -19,17 +19,18 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'centromedicomanuel.com' );
 
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+if(strstr($_SERVER['SERVER_NAME'], 'centromedicomanuel.com.test')) {
+    define( 'DB_NAME', 'centromedicomanuel.com' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', '' );
+    define( 'DB_HOST', 'localhost' );
+} else {
+    define( 'DB_NAME', 'centromedicomanuel' );
+    define( 'DB_USER', 'steve_centromedicomanuel' );
+    define( 'DB_PASSWORD', 'september51995' );
+    define( 'DB_HOST', 'localhost' );
+}
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
