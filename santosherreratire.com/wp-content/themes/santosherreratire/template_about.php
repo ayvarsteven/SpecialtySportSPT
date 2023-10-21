5725c63f2b95be30dd26e0070628851da0f3ea50
Steven Ayvar <ayvarsteven@gmail.com>
2023-10-21 17:40:13 -0500

    completes about us page

--- santosherreratire.com/wp-content/themes/santosherreratire/template_about.php
+++ santosherreratire.com/wp-content/themes/santosherreratire/template_about.php
@@ -6,9 +6,29 @@ Template Name: About Page
 
 <?php get_header(); ?>
 
-<!-- ============================================ -->
-<!--             Side By Side Reverse             -->
-<!-- ============================================ -->
+<!-- ------------------------------ ABOUT HEADER ------------------------------ -->
+
+<section id="content-1450">
+    <div class="cs-container">
+        <div class="cs-content">
+            <span class="cs-topper">Learn More About Santos & Herrera Tire Shop</span>
+            <h2 class="cs-title">
+                SEE WHY MORE DRIVERS TURN TO OUR TEAM
+            </h2>
+            <p class="cs-text">
+                We offer quality tires for less from the brands you trust and road-worthy used tires. No matter what style of truck you drive, we have your perfect set of tires.
+            </p>
+            <p class="cs-text">
+                One of the hardest things about your vehicle is keeping it rolling all day long. When you need the best services for truck tires, more area drivers prefer our store. From new tire sales to pre-used tires, repairs, and more services, visit Truck Tire Depot.
+            </p>
+            <a href="
+                <?php echo site_url("/contact"); ?>
+            " class="cs-button-solid">Speak to An Expert</a>
+        </div>
+    </div>
+</section>
+                                
+<!-- ------------------------------ ABOUT CONTENT ------------------------------ -->
 
 <section id="sbsr-1207">
     <div class="cs-container">
@@ -17,61 +37,47 @@ Template Name: About Page
             <!--To make your own PNG cropped people, use https://www.remove.bg/ to remove the background of any image-->
             <!--Main Image-->
             <picture class="cs-picture">
-                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/accountant2.png">
-                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/accountant2.png">
-                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/accountant2.png" alt="person with box" width="522" height="793"  aria-hidden="true">
+                <source media="(max-width: 600px)" srcset="
+                    <?php echo get_theme_file_uri(
+                        "img/AdobeStock_332201184_Preview.jpeg"
+                    ); ?>
+                ">
+                <source media="(min-width: 601px)" srcset="
+                    <?php echo get_theme_file_uri(
+                        "img/AdobeStock_332201184_Preview.jpeg"
+                    ); ?>
+                ">
+                <img loading="lazy" decoding="async" src="
+                    <?php echo get_theme_file_uri(
+                        "img/AdobeStock_332201184_Preview.jpeg"
+                    ); ?>
+                " alt="tires" width="522" height="793"  aria-hidden="true">
             </picture>
-            <!--Small Image-->
-            <div class="cs-box">
-                <img class="cs-box-icon" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/growth.svg" alt="icon" width="40" height="40" aria-hidden="true">
-                <span class="cs-box-header">Business Growth</span>
-                <ul class="cs-box-ul">
-                    <li class="cs-box-li">Global Industry</li>
-                    <li class="cs-box-li">Smart Solution</li>
-                    <li class="cs-box-li">Fast Process</li>
-                </ul>
-            </div>
-            <div class="cs-stat">
-                <div class="cs-ring-wrapper">
-                    <span class="cs-number">90%</span>
-                    <!--Change the --ratio to the decimal percentage of this circle-->
-                    <svg class="cs-ring" style="--ratio: 0.90">
-                        <circle cx="50%" cy="50%" r="50%"/>
-                        <circle cx="50%" cy="50%" r="50%"/>
-                    </svg>
-                </div>
-                <span class="cs-header">Project Success</span>
-                <span class="cs-desc">by assisted startup</span>
-            </div>
-            <!--Circle Graphic-->
-            <img class="cs-graphic" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Graphics/swirl.svg" alt="graphic" width="950" height="950" aria-hidden="true">
         </div>
         <!-- Right Content Section-->
         <div class="cs-content">
-            <span class="cs-topper">About Us</span>
-            <h2 class="cs-title">Always Ready to Invest in Any Business</h2>
-            <h3 class="cs-h3">We consider all the drivers of change gives you the blocks pride for our components you need to change to create.</h3>
+            <span class="cs-topper">
+                SEE WHY MORE DRIVERS TURN TO OUR TEAM
+            </span>
+            <h2 class="cs-title">Learn More About Santos & Herrera Tire Shop</h2>
+            <h3 class="cs-h3">
+                We offer quality tires for less from the brands you trust and road-worthy used tires. No matter what style of truck you drive, we have your perfect set of tires.
+            </h3>
             <p class="cs-text">
-                In the new era of technology we look in the future with certainty and pride for our company and business. consider all the drivers of change gives you the blocks & pride for our components you need to change to create a truly professional website In the new era.
+                One of the hardest things about your vehicle is keeping it rolling all day long. When you need the best services for truck tires, more area drivers prefer our store. From new tire sales to pre-used tires, repairs, and more services, visit Truck Tire Depot.
             </p>
             <div class="cs-content-flair">
                 <ul class="cs-ul">
-                    <li class="cs-li">Stage-focused investing.</li>
-                    <li class="cs-li">Sector specialization.</li>
-                    <li class="cs-li">Active involvement.</li>
-                    <li class="cs-li">Portfolio diversification.</li>
-                    <li class="cs-li">Achieving a return.</li>
+                    <li class="cs-li">Affordable Pricing</li>
+                    <li class="cs-li">Quality Name-Brand Tires</li>
+                    <li class="cs-li">Certified Safe Used Tires</li>
+                    <li class="cs-li">Experienced Repair Staff</li>
+                    <li class="cs-li">Friendly Sales Team</li>
+                    <li class="cs-li">Professional Tools and Equipment</li>
+                    <li class="cs-li">Convenient Store Location</li>
+                    <li class="cs-li">Tires for Trailers and More</li>
                 </ul>
-                <a href="" class="cs-video-link">
-                    <!--BG Image-->
-                    <img class="cs-video-bg" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/video-bg.jpg" alt="people" width="280" height="160" aria-hidden="true">
-                    <!--Play Icon-->
-                    <picture class="cs-wrapper">
-                        <img class="cs-link-icon" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/play-sharp2.svg" alt="graphic" width="24" height="24" aria-hidden="true">
-                    </picture>
-                </a>
             </div>
-            <a class="cs-button-solid" aria-label="learn more about our programs" href="">Learn More</a>
         </div>
     </div>
 </section>
