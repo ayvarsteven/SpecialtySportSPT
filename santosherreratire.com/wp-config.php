01b81030ac3b9702a5377fc9f33848081c382064
Steven Ayvar <ayvarsteven@gmail.com>
2023-10-21 19:58:26 -0500

    cleans up plugins and adds database credentials for staging and production environments

--- santosherreratire.com/wp-config.php
+++ santosherreratire.com/wp-config.php
@@ -19,17 +19,18 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'santosherreratire.com' );
 
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+if(strstr($_SERVER['SERVER_NAME'], 'santosherreratire.com.test')) {
+    define( 'DB_NAME', 'santosherreratire.com' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', '' );
+    define( 'DB_HOST', 'localhost' );
+} else {
+    define( 'DB_NAME', 'santosherreratire' );
+    define( 'DB_USER', 'steve_santosherreratire' );
+    define( 'DB_PASSWORD', 'september51995' );
+    define( 'DB_HOST', 'localhost' );
+}
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
